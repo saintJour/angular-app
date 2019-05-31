6a35b6ba559a1dfeea61e35b2d5bce30807a1ae0
saintJour <matiasfigueroasj@gmail.com>
2019-05-30 21:01:25 -0400

    add welcome view and fix advanced search tags

--- src/app/components/advanced-search/advanced-search.component.ts
+++ src/app/components/advanced-search/advanced-search.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Document } from 'src/app/models/document.model';
 import { DocumentService } from 'src/app/services/document.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Tag } from 'src/app/models/tag.model';
+import { TagService } from 'src/app/services/tag.service';
+import { HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-advanced-search',
@@ -19,12 +22,20 @@ export class AdvancedSearchComponent implements OnInit {
   ratingFrom: number;
   ratingTo: number;
   ratingValues: number[] = [];
+  tags: any[] = [];
+  providedTags: Tag[];
 
   constructor(
     private actRoute: ActivatedRoute,
     private router: Router,
-    private docSvc: DocumentService
+    private docSvc: DocumentService,
+    private tagSvc: TagService
   ) {
+    this.tagSvc.getAll()
+    .subscribe(tags => {
+      this.providedTags = tags;
+    });
+
     this.actRoute.queryParams
     .subscribe(params => {
       this.name = params['name'] || "";
@@ -33,23 +44,13 @@ export class AdvancedSearchComponent implements OnInit {
       this.ratingValues[0] = Number(params['ratingFrom']) || 1;
       this.ratingValues[1] = Number(params['ratingTo']) || 5;
       this.types = params['types'] || ["note", "book", "exam", "test"];
+      if(params['tags']){
+        params['tags'].forEach(tag => {
+          this.tags.push({id: 0, display: tag})
+        });
+      }
     });
 
-    this.docSvc.filtered({
-      name: this.name,
-      yearFrom: this.yearValues[0],
-      yearTo: this.yearValues[1],
-      ratingFrom: this.ratingValues[0],
-      ratingTo: this.ratingValues[1],
-      types: this.types
-    }).subscribe(docs => {
-      this.documents = docs;
-    });
-   }
-
-  ngOnInit() { }
-
-  doFilter(){
     let params = {
       name: this.name,
       yearFrom: this.yearValues[0],
@@ -58,6 +59,37 @@ export class AdvancedSearchComponent implements OnInit {
       ratingTo: this.ratingValues[1],
       types: this.types
     };
+
+    let sTags = [];
+    this.tags.forEach(tag => {
+      sTags.push(tag.display);
+    });
+    params['tags'] = sTags;
+
+    this.docSvc.filtered(params).subscribe(docs => {
+      this.documents = docs;
+    });
+   }
+
+  ngOnInit() { }
+
+  doFilter(){
+    
+    let params = {
+      name: this.name,
+      yearFrom: this.yearValues[0],
+      yearTo: this.yearValues[1],
+      ratingFrom: this.ratingValues[0],
+      ratingTo: this.ratingValues[1],
+      types: this.types
+    };
+
+    let sTags = [];
+    this.tags.forEach(tag => {
+      sTags.push(tag.display);
+    });
+    params['tags'] = sTags;
+
     this.router.navigateByUrl('', {skipLocationChange: true}).then(() => {
       this.router.navigate(['/advanced-search'], {queryParams: params});
     });
